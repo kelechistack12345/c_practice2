21a5a2b684fa53a221b9920d0a9f1f6e592bab49
kelechistack12345 <57069824+kelechistack12345@users.noreply.github.com>
2022-07-19 17:25:50 -0700

    19/07/2022-5:25

---
+++ printf_function.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// to print out integer
+int main(){
+    printf("%d \n",500 );
+
+// print character in c
+
+    printf("my favourite %s is %d \n","number",500);
+    // print your float and in 2 decimal place
+    printf("my favourite %s is %.2f \n","number",500.25);
+    // print your value when you assign an integer to a character
+    int favnum =90;
+    char mychar = 'i' ;
+    printf("my favourite %s is %d and %c know \n","number",favnum, mychar);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
---
+++ working_with_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+   
+
+    // print decimal numbers
+    printf("%f \n",8.9);
+    // print summed floats
+    printf("%f \n",8.9 + 2.3);
+    // print summed floats and int it return a float number
+     printf("%f \n",8 + 2.3);
+    // print divided int
+     printf("%d \n",8/2);
+    // print divided float
+     printf("%f",8.5/2);
+
+   
+
+
+
+
+    return 0;
+
+}
