3d34f2a8bf7884272bb3392014fa67112ab5fce1
kelechistack12345 <57069824+kelechistack12345@users.noreply.github.com>
2022-07-20 12:30:22 -0700

    20/07/2022-12:30

--- getinputfromuser.c
+++ getinputfromuser.c
@@ -4,26 +4,39 @@
 // input from user for integer
 int main()
 {
-    int age;
-    double value;
-    char grade ;
+    
 
-     printf("enter your age: ");
-    scanf("%d", &age);
-    printf("you are %d: \n" ,age);
-
-
-    // input from user for double
-    printf("enter your value: ");
-    scanf("%lf", &value);
-    printf("you are %f: \n" ,value ); 
+    // int age;
+    //  printf("enter your age: ");
+    //  scanf("%d", &age);
+    //  printf("you are %d \n" ,age);
 
 
-    printf("enter you grade: ");
-    scanf("%c", &grade);
-    printf("you grade in %c: \n" ,grade );
+    // // input from user for double
+    //  double value ;
+    //  printf("enter your value: ");
+    //  scanf("%lf", &value);
+    //  printf("you are %f \n" ,value ); 
 
-   
+     // 
+
+    //  char grade ;
+    //  printf("enter you grade: ");
+    //  scanf("%c", &grade);
+    //  printf("you grade is %c \n" ,grade );
+
+    // the scanf does not recognize any input after a space but we can use an fget function
+    //   char name[20];
+    //   printf("enter you name: ");
+    //   scanf("%s", name);
+    //   printf("you name in %s \n" ,name );
+
+    // the fget function
+       char name[20];
+       printf("enter you name: \n");
+       fgets(name,20,stdin);
+       printf("you name is %s \n" ,name );
+
 
     return 0;
 
