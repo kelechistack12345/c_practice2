576d94dbdf9bef11afca7e0917b8bf2074b70b75
kelechistack12345 <57069824+kelechistack12345@users.noreply.github.com>
2022-07-25 04:37:27 -0700

    25/07/2022-4:37

--- if_statement_for_3_variable.c
+++ if_statement_for_3_variable.c
@@ -18,7 +18,6 @@ int max(int num1,int num2,int num3){
 
      }
      
-
      return result;
 
 }
@@ -30,12 +29,19 @@ int max(int num1,int num2,int num3){
 
 int main(){
 
+    // if(3!=2) to check if 3 is not equal t0 2.
+    // if (3==2) to check if 3 is equal to 2.
 
-    if (3>2 || 2>5){
+
+    if (3<2 || 2>5){
 
         printf("true \n");
-
         
+    }
+    else{
+        
+        printf("false \n");
+
     }
 
     printf("my answer is %d",max(10,20,30));
